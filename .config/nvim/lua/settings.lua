bd00c2ad25032ad7b26dd1af84032eb88022a9d8
Abhinav Srivastava <look4abhinav@outlook.in>
2026-03-07 10:42:09 +0530

    Fixed Neovim Cursor Issue

--- .config/nvim/lua/settings.lua
+++ .config/nvim/lua/settings.lua
@@ -23,7 +23,7 @@ vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.softtabstop = 4
 vim.opt.expandtab = true
-
+vim.opt.guicursor = "n-v-c-i:ver25"
 -- [2] CLIPBOARD (OSC 52)
 -- --------------------------------------------------------------------------
 vim.g.clipboard = {
